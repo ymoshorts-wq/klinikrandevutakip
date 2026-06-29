eb7ab8773e01219b5d1cf648a46ad3265a8539b2
ymoshorts-wq <ymoshorts@gmail.com>
2026-06-29 19:34:25 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.omereymend.klinik',
-  appName: 'Randevu Takip',
-  webDir: 'dist'
-};
-
-export default config;
